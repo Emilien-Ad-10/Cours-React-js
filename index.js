1b97214cc8db66439614063aa6b592f95621c612
Emilien-Ad-10 <BeAd.Git.Hub@proton.me>
2023-01-30 21:54:51 +0100

    La syntaxe JSX avec Babel

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-let CaR = 60;
+/* J A V A S C R I P T */
+{
+
+let CompteARebours = 180;
 
 function render(){
-    const paragrapheCaR = React.createElement('p', {}, 
-    'Compte à rebours : ',
-    React.createElement('span', {}, CaR)    
+    const paragrapheCompteARebours = React.createElement('p', {}, 'Compte à rebours : ',React.createElement('span', {}, CompteARebours)
     );
-    ReactDOM.render(paragrapheCaR, document.querySelector('aside'));
+    ReactDOM.render(paragrapheCompteARebours, document.querySelector('.cas_pratique-1'));
 }
 render();
 
 window.setInterval(() => {
-    CaR--
+    CompteARebours--
     render();    
 }, 1000);
+
+}
+
+/* S Y N T A X E _ J S X */
+{
+let secondes = 0;
+
+function numberFormat(secondes){
+    return secondes.toString().padStart(2, "0");
+}
+
+function render(){
+    const items = ['item 1', 'item 2', 'item 3'];
+    const lis = items.map((item, k) => <li key={k}>{item}</li>)
+    const paragraphesecondes =  <section className="section-jsx">
+                                    <p>timer : <span>{numberFormat(secondes)}</span></p>
+                                    <ul>
+                                        {lis}
+                                    </ul>
+                                </section>
+    ReactDOM.render(paragraphesecondes, document.querySelector('.cas_pratique-2'));
+}
+render();
+    
+window.setInterval(() => {
+    secondes++
+    render();    
+}, 1000);   
+}
